fd58e529cdc6535abb0b4d426e842256c1d892d9
Maximilian <scientificresearch309@gmail.com>
2025-07-29 16:25:57 +0200

    the button works well

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 console.log('Javascript works!');
 
 const messageElem = document.querySelector('.message');
+const btn = document.querySelector('.btn');
 
 messageElem.innerText = 'THIS IS A NEW TEXT!';
 // messageElem.innerHTML = 'THIS IS A NEW TEXT!';
 // messageElem.textContent = 'Abbas123';
 console.log(messageElem);
+
+// Buttton
+btn.addEventListener('click', () => {
+  // console.log('hallo');
+  messageElem.innerText = 'You clicked the button now!';
+});
