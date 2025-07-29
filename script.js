c8c6b8b44f6b813b8acc33b9aafc2ccda7658657
Maximilian <scientificresearch309@gmail.com>
2025-07-29 16:02:57 +0200

    javascript works

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log('Javascript works!');
+
+const messageElem = document.querySelector('.message');
+
+messageElem.innerText = 'THIS IS A NEW TEXT!';
+// messageElem.innerHTML = 'THIS IS A NEW TEXT!';
+// messageElem.textContent = 'Abbas123';
+console.log(messageElem);
