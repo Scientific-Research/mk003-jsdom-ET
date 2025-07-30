791e7da9f454a8e05fe10d1b37848c4e3bc8e6b2
Maximilian <scientificresearch309@gmail.com>
2025-07-30 12:27:55 +0200

    button click chnages the page

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 console.log('Javascript works!');
 
 const messageElem = document.querySelector('.message');
-const btn = document.querySelector('.btn');
+const btnElem = document.querySelector('.btn');
+const textElem = document.querySelector('.text');
 let clicked = false;
 
-messageElem.innerText = 'THIS IS A NEW TEXT!';
+messageElem.innerText = 'Initial Text';
+// textElem.innerText = 'Initial Text';
 // messageElem.innerHTML = 'THIS IS A NEW TEXT!';
 // messageElem.textContent = 'Abbas123';
 console.log(messageElem);
 
 // Buttton
-btn.addEventListener('click', () => {
+btnElem.addEventListener('click', () => {
   // console.log('hallo');
 
   if (!clicked) {
-    messageElem.innerText = 'You clicked the button now!';
-    btn.textContent = 'Clicked!';
+    messageElem.innerHTML = 'You clicked the <strong>button</strong> now!';
+    btnElem.textContent = 'Clicked!';
     clicked = true;
     console.log(clicked);
   } else {
-    messageElem.innerText = 'THIS IS A NEW TEXT!';
-    btn.textContent = 'UnClicked!';
+    messageElem.innerText = '<b>Initial</b> Text!';
+    btnElem.textContent = 'UnClicked!';
     clicked = false;
     console.log(clicked);
   }
